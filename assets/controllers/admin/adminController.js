2d17e857c1410ebc14083a5129e46f7ccd4bcd43
GabyLim20 <gabriela.g.satoritech@gmail.com>
2026-05-20 22:44:12 -0600

    Debug admin

--- assets/controllers/admin/adminController.js
+++ assets/controllers/admin/adminController.js
@@ -1,11 +1,11 @@
 const { getAllStudents, createStudent, updateStudent, deleteStudent, getStudentAttendanceHistory } = require("./studentController");
 const { getAllTeachers, createTeacher, updateTeacher, deleteTeacher } = require("./teacherController");
-const { getClassesTable, createClass, updateClass, deleteClass }     = require("./classController");
-const { listSubjects, createSubject }                                = require("./subjectController");
-const { getDashboardStats }                                           = require("./dashboardController");
-const { getAttendanceRiskStatistics }                                 = require("./statisticsController");
-const { getCatalogs, listSections }                                   = require("./catalogController");
-const { uploadStudentExcel }                                          = require("../uploadController");
+const { getClassesTable, createClass, updateClass, deleteClass } = require("./classController");
+const { listSubjects, createSubject } = require("./subjectController");
+const { getDashboardStats } = require("./dashboardController");
+const { getAttendanceRiskStatistics } = require("./statisticsController");
+const { getCatalogs, listSections } = require("./catalogController");
+const { uploadStudentExcel } = require("../uploadController");
 const { enrollSection, enrollStudent, deleteEnrollment, getStudentEnrollments, getEnrollmentsByCourse, syncEnrollmentsWithSection } = require("./enrollmentController");
 
 module.exports = {
